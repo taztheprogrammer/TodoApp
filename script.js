0188254585f96478af50cf671d8dd2d55788a4f1
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2023-11-02 11:27:29 -0400

    Placed 4 item divs in itemHolder manually
    Used js to give names from item objects

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const item1 = {
-    title: "cse214"
+const i1 = document.getElementById("i1");
+const i2 = document.getElementById("i2");
+const i3 = document.getElementById("i3");
+const i4 = document.getElementById("i4");
+
+function Class(name) {
+    this.name = name;
 }
 
-const i1 = document.getElementById("i1");
+const c1 = new Class("cse214");
+const c2 = new Class("cse215");
+const c3 = new Class("ams301");
+const c4 = new Class("che131");
 
-i1.innerHTML = item1.title
+const items = [i1, i2, i3, i4]
+const classes = [c1, c2, c3, c4]
+
+for (let i = 0; i < 4; i++) {
+    items[i].innerHTML = classes[i].name;
+}
