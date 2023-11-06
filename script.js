5be3ef500ef44c08beb83f74e0059313d2586f4c
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2023-11-06 09:02:28 -0500

    Utilized simple JS for loop to add elements to dom

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const i1 = document.getElementById("i1");
-const i2 = document.getElementById("i2");
-const i3 = document.getElementById("i3");
-const i4 = document.getElementById("i4");
+// const i1 = document.getElementById("i1");
+// const i2 = document.getElementById("i2");
+// const i3 = document.getElementById("i3");
+// const i4 = document.getElementById("i4");
 
-function Class(name) {
-    this.name = name;
+// function Class(name) {
+//     this.name = name;
+// }
+
+// const c1 = new Class("cse214");
+// const c2 = new Class("cse215");
+// const c3 = new Class("ams301");
+// const c4 = new Class("che131");
+
+// const items = [i1, i2, i3, i4]
+// const classes = [c1, c2, c3, c4]
+
+// for (let i = 0; i < 4; i++) {
+//     items[i].innerHTML = classes[i].name;
+// }
+
+
+
+
+const itemholder = document.getElementById('itemHolder');
+const items = ['i1', 'i2', 'i3'];
+
+
+function render() {
+    for(let num = 0; num < items.length; num++) {
+        const item = document.createElement('div');
+        item.classList.add('item');
+        item.innerHTML = items[num];
+        itemholder.appendChild(item);
+    }
 }
 
-const c1 = new Class("cse214");
-const c2 = new Class("cse215");
-const c3 = new Class("ams301");
-const c4 = new Class("che131");
-
-const items = [i1, i2, i3, i4]
-const classes = [c1, c2, c3, c4]
-
-for (let i = 0; i < 4; i++) {
-    items[i].innerHTML = classes[i].name;
-}
+render();
